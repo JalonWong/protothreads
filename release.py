f72b7c1f785f188e2d2613f1a4494f8b532f4b33
Jalon Wong <jalonwong@gmail.com>
2025-12-03 14:39:17 -0600

    Simplify module

--- release.py
+++ release.py
@@ -1,3 +1,4 @@
+import re
 import sys
 import tarfile
 from glob import glob
@@ -13,10 +14,13 @@ bazel_dep(name = "protothreads", version = "{version}")
 if __name__ == "__main__":
     tag = sys.argv[1]
 
-    v = tag.replace("v", "")
-    print(TAMPLATE.format(version=v))
-    with open("MODULE.bazel", "w") as f:
-        f.write(f'module(name = "protothreads", version = "{v}", compatibility_level = 1)\n\n')
+    with open("MODULE.bazel", "r") as f:
+        v = tag.replace("v", "")
+        print(TAMPLATE.format(version=v))
+        text = f.read().replace("0.0.0", v)
+        text = re.sub(r"bazel_dep\(.+dev_dependency = True\)\n", "", text)
+        with open("MODULE.bazel", "w") as f:
+            f.write(text)
 
     with tarfile.open(f"protothreads-{tag}.tar.gz", "w:gz") as tar:
         tar.add(".bazelrc")
